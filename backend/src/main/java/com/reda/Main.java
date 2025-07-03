a597af9ae670ded7fd9162008d747091166b49fb
Reda-Kriate <kriatereda@gmail.com>
2025-07-03 20:15:27 +0100

    ajouter une ligne vide et supprimer un import inutile

--- backend/src/main/java/com/reda/Main.java
+++ backend/src/main/java/com/reda/Main.java
@@ -4,19 +4,17 @@ import com.github.javafaker.Faker;
 import com.github.javafaker.Name;
 import com.reda.customer.Customer;
 import com.reda.customer.CustomerRepository;
-import org.aspectj.weaver.ast.Var;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-import java.util.List;
 import java.util.Random;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class,args);
+
     }
     @Bean
     CommandLineRunner runner(CustomerRepository customerRepository){

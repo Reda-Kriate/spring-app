3c4d1a888f05c5d03cc39ff0c33c1a12b4cf6ee9
Reda-Kriate <kriatereda@gmail.com>
2025-07-08 17:18:21 +0100

    hbibi v10

--- backend/src/main/java/com/reda/De3bol.java
+++ backend/src/main/java/com/reda/De3bol.java
@@ -10,6 +10,6 @@ class De3bolController {
 
     @GetMapping("/de3bol")
     public De3bol getDe3bol(){
-        return new De3bol("hbiiib gelbiiiiiiiii 7ammoud anas mzaater et de3bol lkbir 🥰");
+        return new De3bol("hbiiiib gelbiiiiiiiii 7ammoud anas mzaater et de3bol lkbir 🥰");
     }
 }

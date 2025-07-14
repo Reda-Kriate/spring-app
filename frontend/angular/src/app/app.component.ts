69518dd23e7d96ff9af104d88763cd79fe5d0679
Reda-Kriate <kriatereda@gmail.com>
2025-07-14 20:47:14 +0100

    exemple routing dynamic et static avec Angular

--- frontend/angular/src/app/app.component.ts
+++ frontend/angular/src/app/app.component.ts
@@ -1,20 +1,27 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Router, RouterLink, RouterOutlet} from '@angular/router';
 import { FirstComponent } from './first/first.component';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ FirstComponent, FormsModule],
+  imports: [FirstComponent, FormsModule, RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular';
   result : number = 0;
+  constructor(
+    private router:Router
+  ) {}
 
   changeResult(res: number) {
     this.result = res;
+  }
+
+  clickRoute() {
+    this.router.navigate(['page-3']);
   }
 }

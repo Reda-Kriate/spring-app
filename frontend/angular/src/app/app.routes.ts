91641354bc38b52c97294be0cdd2cd26fda4c59b
Reda-Kriate <kriatereda@gmail.com>
2025-07-17 14:30:25 +0100

    debut d'impl angular fornt v4

--- frontend/angular/src/app/app.routes.ts
+++ frontend/angular/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {CustomerComponent} from './components/customer/customer.component';
+import {LoginComponent} from './components/login/login.component';
 
 
 
 export const routes: Routes = [
-  {path:"customer" , component:CustomerComponent}
+  {path:"customer" , component:CustomerComponent},
+  {path:"login" , component:LoginComponent}
 ];
--- frontend/angular/src/app/components/login/login.component.ts
+++ frontend/angular/src/app/components/login/login.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
+import {Avatar} from 'primeng/avatar';
 import {InputText} from 'primeng/inputtext';
 import {ButtonDirective} from 'primeng/button';
 
 @Component({
-  selector: 'app-management-customer',
+  selector: 'app-login',
   imports: [
+    Avatar,
     InputText,
     ButtonDirective
   ],
-  templateUrl: './management-customer.component.html',
-  styleUrl: './management-customer.component.scss'
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
 })
-export class ManagementCustomerComponent {
+export class LoginComponent {
 
 }

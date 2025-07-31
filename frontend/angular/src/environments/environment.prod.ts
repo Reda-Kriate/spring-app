9950e205d9bc1b845280bef2a6e7239b3bcafa94
Reda-Kriate <kriatereda@gmail.com>
2025-07-31 20:08:01 +0100

    fix issues to deploy front end -> aws amplify

---
+++ frontend/angular/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  api : {
+    baseUrl : 'http://springapp-reda-env.eba-prkbqefy.us-east-1.elasticbeanstalk.com',
+    authUrl : 'api/v1/auth/login',
+    customerUrl : 'api/v1/customer'
+  }
+};
